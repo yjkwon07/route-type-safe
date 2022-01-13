883ff32c56181c1caef510a608d95ee5f3009f93
yjkwon07 <dudwns9073@nate.com>
2022-01-13 11:11:32 +0900

    [utils]: transformer 타입변환 함수

---
+++ src/transformer.ts
@@ -0,0 +1,69 @@
+const stringTransformer = (value: string): string => {
+  return value;
+};
+
+const numberTransformer = (value: string): number => {
+  const result = Number(value);
+
+  if (Number.isNaN(result)) {
+    throw new Error(`Failed to convert ${value} to number`);
+  }
+
+  return result;
+};
+
+const booleanTransformer = (value: string): boolean => {
+  if (value === 'true') return true;
+  if (value === 'false') return false;
+
+  throw new Error(`Failed to convert ${value} to boolean`);
+};
+
+const dateTransformer = (value: string): Date => {
+  const date = new Date(value);
+
+  if (Number.isNaN(date.getTime())) {
+    throw new Error(`Couldn't transform ${value} to date`);
+  }
+
+  return date;
+};
+
+const oneOfTransformer = <T extends (string | number | boolean)[]>(...values: T) => {
+  return (value: string): T[number] => {
+    for (const canonicalValue of values) {
+      switch (typeof canonicalValue) {
+        case 'string':
+          if (stringTransformer(value) === canonicalValue) return canonicalValue;
+          break;
+        case 'number':
+          if (numberTransformer(value) === canonicalValue) return canonicalValue;
+          break;
+        case 'boolean':
+          if (booleanTransformer(value) === canonicalValue) return canonicalValue;
+          break;
+        default:
+          break;
+      }
+    }
+
+    throw new Error(`No matching value for ${value}`);
+  };
+};
+
+const arrayOfTransformer = <T>(transformer: (value: string) => T) => {
+  return (value: string[]): T[] => {
+    return value.map((item) => transformer(item));
+  };
+};
+
+const transformer = {
+  string: stringTransformer,
+  number: numberTransformer,
+  boolean: booleanTransformer,
+  date: dateTransformer,
+  oneOf: oneOfTransformer,
+  arrayOf: arrayOfTransformer,
+};
+
+export default transformer;
